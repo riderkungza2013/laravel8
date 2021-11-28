f3c40d2c3554228f900e80da036d188b46aab437
riderkungza2013 <finalfantasy028@gmail.com>
2021-11-28 16:53:48 +0700

    Week 2

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,8 @@
+<html>
+
+<body>
+    <h1>Hello, {{ $name }} {{ $last_name}} </h1>
+    <!-- <h1>Hello, <?php echo $name; ?>  <?php echo  $last_name; ?></h1> -->
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/homepage", function () {
+    return "<h1>This is home page</h1>";
+});
+
+Route::get("/blog/{id}", function ($id) {
+    return "<h1>This is blog page : {$id} </h1>";
+});
+Route::get("/blog/{id}/edit", function ($id) {
+    return "<h1>This is blog page : {$id} for edit</h1>";
+});
+Route::get("/product/{a}/{b}/{c}", function ($a, $b, $c) {
+    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>";
+});
+Route::get("/category/{a?}", function ($a = "mobile") {
+    return "<h1>This is category page : {$a} </h1>";
+});
+Route::get("/myorder/create", function () {
+    return "<h1>This is order form page : " . request("username") . "</h1>";
+});
+
+Route::get("/hello", function () {
+    return view("hello");
+});
+
+Route::get('/greeting', function () {
+
+    $name = 'James';
+    $last_name = 'Mars';
+
+    return view('greeting', compact('name', 'last_name'));
+});
